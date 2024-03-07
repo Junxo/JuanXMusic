e7a34e62969902258c0706cf305b18e87d74b901
Junxo <146384707+Junxo@users.noreply.github.com>
2024-03-08 02:38:17 +0700

    Update ping.py

--- JuanXMusic/plugins/tools/ping.py
+++ JuanXMusic/plugins/tools/ping.py
@@ -1,87 +1,14 @@
-import random
-
-import asyncio
-
-from asyncio import sleep
 from datetime import datetime
-from pyrogram import filters
 
+from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.errors import FloodWait, UserNotParticipant
 
 from JuanXMusic import app
-
 from JuanXMusic.core.call import Anony
 from JuanXMusic.utils import bot_sys_stats
 from JuanXMusic.utils.decorators.language import language
 from JuanXMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
-
-from JuanXMusic import *
-
-
-
-
-spam_chats = []
-emoji = "😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 😍 🤩 🥳 🤗 🙃 🙂 ☺️ 😊 😏 😌 😉 🤭 😶 😐 😑 😔 😋 😛 😝 😜 🤪 🤔 🤨 🧐 🙄 😒 😤 😠 🤬 ☹️ 🙁 😕 😟 🥺 😳 😬 🤐 🤫 😰 😨 😧 😦 😮 😯 😲 😱 🤯 😢 😥 😓 😞 😖 😣 😩 😫 🤤 🥱 😴 😪 🌛 🌜 🌚 🌝 🎲 🧩 ♟ 🎯 🎳 🎭💕 💞 💓 💗 💖 ❤️‍🔥 💔 🤎 🤍 🖤 ❤️ 🧡 💛 💚 💙 💜 💘 💝 🐵 🦁 🐯 🐱 🐶 🐺 🐻 🐨 🐼 🐹 🐭 🐰 🦊 🦝 🐮 🐷 🐽 🐗 🦓 🦄 🐴 🐸 🐲 🦎 🐉 🦖 🦕 🐢 🐊 🐍 🐁 🐀 🐇 🐈 🐩 🐕 🦮 🐕‍🦺 🐅 🐆 🐎 🐖 🐄 🐂 🐃 🐏 🐑 🐐 🦌 🦙 🦥 🦘 🐘 🦏 🦛 🦒 🐒 🦍 🦧 🐪 🐫 🐿️ 🦨 🦡 🦔 🦦 🦇 🐓 🐔 🐣 🐤 🐥 🐦 🦉 🦅 🦜 🕊️ 🦢 🦩 🦚 🦃 🦆 🐧 🦈 🐬 🐋 🐳 🐟 🐠 🐡 🦐 🦞 🦀 🦑 🐙 🦪 🦂 🕷️ 🦋 🐞 🐝 🦟 🦗 🐜 🐌 🐚 🕸️ 🐛 🐾 🌞 🤢 🤮 🤧 🤒 🍓 🍒 🍎 🍉 🍑 🍊 🥭 🍍 🍌 🌶 🍇 🥝 🍐 🍏 🍈 🍋 🍄 🥕 🍠 🧅 🌽 🥦 🥒 🥬 🥑 🥯 🥖 🥐 🍞 🥜 🌰 🥔 🧄 🍆 🧇 🥞 🥚 🧀 🥓 🥩 🍗 🍖 🥙 🌯 🌮 🍕 🍟 🥨 🥪 🌭 🍔 🧆 🥘 🍝 🥫 🥣 🥗 🍲 🍛 🍜 🍢 🥟 🍱 🍚 🥡 🍤 🍣 🦞 🦪 🍘 🍡 🥠 🥮 🍧 🍨".split(
-    " "
-)
-
-
-def get_arg(client, message: Message, _):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
-
-@app.on_message(filters.command(["tall"]) & filters.private & ~BANNED_USERS)
-@language
-async def tagall(client, message: Message, _):
-    await message.delete()
-    chat_id = message.chat.id
-    args = get_arg(message)
-    if not args:
-        args = "Hi!"
-    spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ""
-    m = client.get_chat_members(chat_id)
-    async for usr in m:
-        if not chat_id in spam_chats:
-            break
-        usrnum += 1
-        usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.user.id}) "
-        if usrnum == 5:
-            txt = f"**{args}**\n\n{usrtxt}"
-            try:
-                await client.send_message(chat_id, txt)
-            except FloodWait as e:
-                await sleep(e.value)
-                await client.send_message(chat_id, txt)
-
-            await sleep(2)
-            usrnum = 0
-            usrtxt = ""
-    try:
-        await client.send_message(chat_id, "**Proses Tag All selesai.")
-        spam_chats.remove(chat_id)
-    except:
-        pass
-
-@app.on_message(filters.command(["cancel"]) & filters.private & ~BANNED_USERS)
-@language
-async def untag(client, message: Message, _):
-    if not message.chat.id in spam_chats:
-        return await message.reply("**Sepertinya tidak ada tagall disini.**")
-    else:
-        try:
-            spam_chats.remove(message.chat.id)
-        except:
-            pass
-        return await message.reply("**Proses Tag All berhenti..**")
 
 
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
