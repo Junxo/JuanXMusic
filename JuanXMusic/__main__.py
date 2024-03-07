04db21c1693a8337cee2e4879931eca21fe56c34
Junxo <146384707+Junxo@users.noreply.github.com>
2024-03-08 00:05:40 +0700

    Update __main__.py

--- JuanXMusic/__main__.py
+++ JuanXMusic/__main__.py
@@ -42,7 +42,7 @@ async def init():
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("Geez|RAM").error(
+        LOGGER("JuanXMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
